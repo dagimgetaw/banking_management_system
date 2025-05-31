ffa49713deebcf8bf7531ac8090f2a613bfc230b
Dagim <dagimgetaW27@gmail.com>
2025-05-31 12:30:40 +0300

    feat: add img asset

---
+++ src/banking/Main.java
@@ -0,0 +1,9 @@
+package banking;
+
+import banking.auth.Login;
+
+public class Main {
+    public static void main(String[] args) {
+        new Login(); // Show login window when app starts
+    }
+}
